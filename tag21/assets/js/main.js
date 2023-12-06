2778aeb5aa3e0d9c8beabe212167eac07e6746ce
MagdaCale <magdacale@gmail.com>
2023-12-06 10:55:06 +0100

    JavaScript Einführung: Basics

--- tag21/assets/js/main.js
+++ tag21/assets/js/main.js
@@ -3,6 +3,7 @@ console.log("Hello World!");
 // mit console.log() können wir uns Texte (strings) ausgeben lassen.
 
 // ! ===== Variable =====
+console.log("===== Variable =====");
 
 // - const - wird für Werte verwendet die nicht verändert werden sollen
 // - let - Werte können noch mal verändert werden
@@ -52,3 +53,78 @@ const addScore = num1 + num2
 console.log({addScore});
 
 console.log("TEST ",num1 + num2);
+
+
+// ! ===== DatenType =====
+console.log("===== DatenType =====");
+
+// ? Number - String - Boolean
+
+// ? String
+// String sind Textwerte 
+// Um ein String auszugeben, müssen wir "" oder '' schreiben
+
+console.log("Das ist ein String");
+console.log('Das ist auch ein String');
+// wird in der Konsole als weißer Text ausgegeben 
+
+// ? Number
+// numerische Werte und werden lila ausgegeben
+console.log('NUMBERS');
+console.log(10);
+console.log(10 + 4); // gibt 14 aus
+
+console.log("10" + "4"); // führt eine Zeichenketten-konkatenation durch
+console.log("Vier " * 4); // kommt NaN raus - NotaNumber
+console.log("4 " * 4); // kommt 16 raus
+console.log("4" * "4");
+console.log("10" - "4");
+
+// ? Boolean
+// werden ebenfalls lila ausgegebn und sie haben nur zwei möglich Werte: true oder false
+console.log(true);
+console.log(false);
+
+
+// ! ===== Konvertionen =====
+
+// ! selbsterklärende Namen verwenden
+// * nicht zu kurz
+// let meineFunktion = "funktion"
+
+// * nicht zu lang
+// let lassUnsGemeinsamJavaScriptLernenDennJSIstCool = "NIEN; NICHT SO!"
+
+// * dürfen nicht mit Zahlen anfangen
+// let 2Pac = "Nein, nein!"
+// statdessen let twoPac = "so ist es besser!"
+
+// * camelCase
+
+// - BestPractise
+
+// * camelCase
+let meineErsteVariable;
+// Englisch ist besser
+let myFirstVariable;
+
+// Underscoure
+let my_forst_variable;
+
+// PascalCase
+let MyFirstVariable;
+
+// # Was nicht geht:
+// # let nein-nein-so-nicht
+// # let $bitteSoAuchNicht;
+// # let kannichsonichtlesen
+
+
+// ! ===== Case Sensitive =====
+
+// das sind zwei unterschiedliche Werte
+let a = 10;
+let A = 100;
+console.log({a});
+console.log({A});
+
