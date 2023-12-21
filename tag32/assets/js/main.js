72770fde96220b7846b57adc53533a72634266fe
FaridTvK <farid@super-code.de>
2023-12-21 11:50:26 +0100

    Dom_1

--- tag32/assets/js/main.js
+++ tag32/assets/js/main.js
@@ -89,12 +89,108 @@ const lastChildVonMeinemListe = liste.lastElementChild.style.color = "red"
 
 const geschwisterElementVomAnfang = liste.lastElementChild.previousElementSibling.innerHTML
 
-
-
 console.log(geschwisterElementVomAnfang);
-
-
 
 console.log(lastChildVonMeinemListe);
 
-lastChildVonMeinemListe
+
+// * HTMLCollection
+
+const liElementeToFastArray = liste.children
+
+console.log("liElementeToArray", liElementeToFastArray);
+
+
+
+// wir haben hier ein fast Array DatenTyp zu einem Array umgewandelt
+
+const liElementeToArrayUmwandeln = Array.from(liste.children)
+
+console.log("liElementeToArrayUmwandeln", liElementeToArrayUmwandeln);
+
+
+// liElementeToFastArray.forEach((singleElement) => {
+//     singleElement.style.color = "purple"
+// })
+
+liElementeToArrayUmwandeln.forEach((singleElement) => {
+    singleElement.style.color = "purple"
+})
+
+liElementeToArrayUmwandeln[0].innerHTML = "Salat"
+liElementeToArrayUmwandeln[0].style.color = "red"
+
+
+const zugriffAufDasParentElement = liste.parentElement
+console.log("zugriffAufDasParentElement", zugriffAufDasParentElement);
+
+
+zugriffAufDasParentElement.style.border = "1px solid black"
+
+// childElementCount length
+
+console.log(liste.childElementCount);
+console.log(liste.children.length);
+
+
+const fließBand = document.querySelector("#fließband")
+
+const firstAuto = document.createElement("li")
+firstAuto.textContent = "Benz"
+console.log(firstAuto);
+
+const secondAuto = document.createElement("li")
+secondAuto.textContent = "Tesla"
+
+const thirdAuto = document.createElement("li")
+thirdAuto.textContent = "BMW"
+
+// push() == unshift()
+fließBand.appendChild(firstAuto)
+fließBand.appendChild(secondAuto)
+
+// prepend() ist wie unshift() methode bei Arrays
+fließBand.prepend(thirdAuto)
+
+
+let imageVomDom = document.createElement("img")
+
+imageVomDom.setAttribute("src", "https://unsplash.it/200")
+imageVomDom.setAttribute("alt", "image aus dem Web")
+
+document.body.prepend(imageVomDom)
+
+
+
+let imageVomDom2 = document.createElement("img")
+imageVomDom2.setAttribute("src", "https://unsplash.it/200")
+fließBand.appendChild(imageVomDom2)
+
+
+if (liElementeToArrayUmwandeln[0].style.color === "green") {
+    liElementeToArrayUmwandeln[0].textContent = "zwiebeln"
+}
+
+
+// Widerholung 
+
+
+let empty = document.createElement("div")
+
+empty.style.height = "200px"
+empty.style.width = "200px"
+empty.style.backgroundColor = "green"
+
+document.body.appendChild(empty)
+
+empty.addEventListener("mouseover", () => {
+    empty.style.backgroundColor = "red"
+})
+
+function changeColor() {
+    empty.style.backgroundColor = "orange"
+}
+
+empty.addEventListener("mouseout", changeColor)
+
+// bitte als zweite Parameter wo man die Function übergeben soll, sollte ohne runde Klammer geschreiben werden
