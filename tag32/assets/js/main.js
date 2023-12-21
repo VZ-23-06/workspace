3dabffc12f362e41db9b49c79e7a2e15c9fc50c4
FaridTvK <farid@super-code.de>
2023-12-21 09:47:55 +0100

    DOM_1

---
+++ tag32/assets/js/main.js
@@ -0,0 +1,41 @@
+// * EVENT & EVENTLISTENER
+
+// Aufbau 
+// 1. Welches Element soll beobachtet werden
+
+// 2 EventListener hinzufügen
+// myBtn.addEventListener()
+
+
+// 3 Event definieren
+// myBtn.addEventListener("click")
+
+const myBtn = document.querySelector("#say-hi-btn")
+const helloOutput = document.querySelector("#hello_output")
+
+myBtn.addEventListener("click", () => {
+    helloOutput.innerHTML = "Hallo vom EventListener"
+})
+
+
+// * ====MouseOver=====
+
+const textInput = document.querySelector("#text-input")
+textInput.addEventListener("mouseover", () => {
+    console.log("Die Maus wurde über mich bewegt");
+    textInput.style.border = "3px solid red"
+})
+
+
+// * =====MouseOut=====
+
+textInput.addEventListener("mouseout", () => {
+    console.log("Die Maus hat mich verlassen");
+    textInput.style.border = "3px solid blue"
+})
+
+// -Alte schreibweise
+// textInput.addEventListener("mouseout", function () {
+//   console.log("Die Maus hat mich verlassen");
+//   textInput.style.border = "3px solid blue";
+// });
