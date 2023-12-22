d21bd514140abe9af37572e5654df5240fe8bdfa
FaridTvK <farid@super-code.de>
2023-12-22 09:02:37 +0100

    Aufgaben

---
+++ tag33/assets/js/main.js
@@ -0,0 +1,148 @@
+// * AUFGABEN
+
+// 1.Schreibe eine Funktion names "durchschnittlichesSchnitDerAbitur", die ein Array von Noten akzeptiert, uind den Durchschnitt der Noten Berechnet. Die Funktion soll jeden Wert im Array aufsummieren und anschließen durch die Anzahl der Elemente im Array teilen. (So bekommen wir den Durchschnitt)
+
+console.log(
+    "%c Aufgabe 1 - durchschnittlichesSchnitDerAbitur",
+    "color: red; background-color: gold"
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2.Übergeben Sie eine Parameter(zahl) im Funktionsaufruf und lassen Sie diese zu einer summe addieren
+console.log(
+    "%c Aufgabe 2 - Übergeben Sie eine Parameter(zahl) im Funktionsaufruf und lassen Sie diese zu einer summe addieren",
+    "color: red; background-color: gold"
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3. For Loop mit String, Wie oft kommen die vokabeln im Satzt vor?
+let buchstabenArray = ["a", "e", "i", "o", "u"];
+console.log(
+    "%c Aufgabe 3 - Buchstaben zählen - for loop",
+    "color: red; background-color: gold"
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 4. Tanary Operator - Check Alter
+console.log(
+    "%c Aufgabe 4 - Tanary Operator - Check Alter",
+    "color: red; background-color: gold"
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 5. Tanary Operator - Check ob eine zahl zwischen 1-10 ist
+console.log(
+    "%c Aufgabe 5 - Tanary Operator - Check Zahl",
+    "color: red; background-color: gold"
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 6. Tanary Operator - Seitenzutritt auf Malboro wenn 18 oder älter
+console.log(
+    "%c Aufgabe 6 - Tanary Operator - Check Zahl",
+    "color: red; background-color: gold"
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 7. map - string to Uppercase ======
+console.log(
+    "%c Aufgabe 7 - map - string to Uppercase",
+    "color: red; background-color: gold"
+);
+let namesArray = ["Magda", "Johanna", "Jannik", "Lisa"];
