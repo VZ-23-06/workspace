3304cacb1ce8d22a9490e33579ad047481b1a4aa
FaridTvK <farid@super-code.de>
2024-01-02 11:12:14 +0100

    object

--- tag34/assets/js/main.js
+++ tag34/assets/js/main.js
@@ -99,7 +99,213 @@ const spieler = {
 
 
 console.log(spieler);
-spieler.login()
-spieler.castSpell()
-spieler.logOut()
+// spieler.login()
+// spieler.castSpell()
+// spieler.logOut()
 
+
+// Obekten Mehtoden => Object.keys()
+
+const keysVomSpielerObject = Object.keys(spieler)
+
+// console.log(keysVomSpielerObject);
+
+// Object.values()
+
+const valuesVomSpielerObject = Object.values(spieler)
+// console.log(valuesVomSpielerObject);
+
+
+keysVomSpielerObject.forEach((key) => {
+    // console.log("key", key);
+    const myValue = spieler[key]
+    // console.log("myValue", myValue);
+})
+
+// const beispiel = spieler.forEach()
+
+// Object.entries()
+// Object.entries erstellt uns ein Array, dadurch können rüber mappen z.B
+const entriesObjectDieKeysUndValuesAlsArray = Object.entries(spieler)
+
+// console.log(entriesObjectDieKeysUndValuesAlsArray);
+
+
+// bei Arrays hatten For-of Loop 
+
+// For-In Loop
+
+for (let akey in spieler) {
+    console.log(akey);
+}
+
+
+const grades = {
+    math: 4,
+    sport: 1,
+    informatik: 2,
+    chemie: 1
+}
+
+let summe = 0;
+
+for (let fach in grades) {
+    summe += grades[fach]
+}
+
+
+console.log(summe);
+
+console.log(grades.length);
+
+const gradesLength = Object.values(grades).length
+
+console.log(gradesLength);
+
+const durchsnitt = summe / gradesLength
+
+console.log(durchsnitt);
+
+
+
+// spread Operator
+
+const user2 = {
+    name: "Hannah",
+    age: 30,
+}
+
+console.log(user2);
+
+const updatedUser = { ...user2, age: 33, email: "hannah@gmail.de" }
+
+console.log(updatedUser);
+
+
+
+// bs2
+
+const user1 = {
+    name: "Alice",
+    age: 30
+}
+
+const mehrInfoÜberUser1 = {
+    adresse: {
+        street: "123 MainSt",
+        city: "Wonderland"
+    }
+}
+
+console.log(user1);
+console.log(mehrInfoÜberUser1);
+
+const user1mitMehrInfoÜberUser1zusammenführen = { ...user1, ...mehrInfoÜberUser1 }
+
+console.log(user1mitMehrInfoÜberUser1zusammenführen);
+
+const einArrayVomUnserObject = Object.entries(user1mitMehrInfoÜberUser1zusammenführen)
+
+console.log(einArrayVomUnserObject);
+
+
+const someArray = [1, 2, 3, 4, 5]
+console.log(someArray);
+
+const reversedArray = [...someArray].reverse()
+console.log(reversedArray);
+
+const nochmalKopie = [...reversedArray].shift()
+
+console.log(nochmalKopie);
+
+
+
+
+const movies = [
+    {
+
+    },
+    {
+
+    }, {
+
+    }
+]
+
+
+let author = {
+    firstName: "Kim",
+    lastName: "Mustermann",
+    book1: {
+        nameBook: "Der Schatz",
+        price: {
+            ebook: {
+                dollar: 19.99,
+                euro: 20,
+            },
+            hardcover: {
+                dollar: 29.99,
+                euro: 20,
+            },
+            paparback: {
+                dollar: 19.99,
+                euro: 20,
+            }
+        }
+    },
+    book2: {
+        nameBook: "mein König",
+        price: {
+            ebook: {
+                dollar: 12.99,
+                euro: 10,
+            },
+            hardcover: {
+                dollar: 29.99,
+                euro: 20,
+            },
+            paparback: {
+                dollar: 19.99,
+                euro: "20$",
+            }
+        }
+    }
+}
+
+console.log(author.book2.price.hardcover.dollar);
+
+author.book1.nameBook = "das Gold"
+
+
+
+const dollarZeichenWegMachen = author.book2.price.paparback.euro.replace("$", "")
+
+console.log(dollarZeichenWegMachen);
+
+console.log(Number(dollarZeichenWegMachen));
+
+console.log(author);
+
+
+
+const gegner = {
+    name: "rambo",
+    fähigkeiten: ["boxen", "arm drücken", "karaten"],
+    logIn: () => {
+        console.log(`${gegner.name} ist eingeloggt`);
+    }
+}
+
+// gegner.logIn()
+
+
+const myBtn = document.createElement("button")
+
+document.body.appendChild(myBtn)
+
+myBtn.textContent = "gegner"
+
+// console.log(myBtn);
+
+myBtn.addEventListener("submit", gegner.logIn(() => { return false }))
